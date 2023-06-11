25b0c2a2e0fb73568dcae35994559d59d3d78a40
Maxence Lesage <maxencelesage4@gmail.com>
2023-06-11 18:50:03 +0200

    navbar + hero responsive

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,8 +1,17 @@
 import "./index.scss";
 import NavCircle from "../navcircle/index";
 import Button from "../../components/button/index";
+import { useRef } from "react";
 
 function Navbar() {
+
+    const refBurger = useRef(null);
+    const refLinks = useRef(null);
+
+    function handleClick() {
+        refBurger.current.classList.toggle("navbar_burger_active");
+        refLinks.current.classList.toggle("burger_navbar");
+    }
 
     return (
         <nav id="navbar">
@@ -15,7 +24,7 @@ function Navbar() {
                 </a>
             </div>
 
-            <div className="navbar__wrapper_right">
+            <div className="navbar__wrapper_right" ref={refLinks}>
                 <div className="navbar_links">
                     <a className="heading" href="#projects">
                         <p>Projets</p>
@@ -29,6 +38,11 @@ function Navbar() {
                 </div>
             </div>
 
+            <button className="navbar_burger" ref={refBurger} onClick={handleClick}>
+                <div className="burger_line" />
+                <div className="burger_line" />
+                <div className="burger_line" />
+            </button>
         </nav>
     );
 }
