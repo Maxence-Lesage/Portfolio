1a1838adbda6245cabb611a4101954cc4b7bc505
Maxence Lesage <maxencelesage4@gmail.com>
2023-08-01 12:30:05 +0200

    responsive graph

--- src/components/chart_bar/index.jsx
+++ src/components/chart_bar/index.jsx
@@ -1,15 +1,16 @@
+import { useState } from "react";
 import TechnoIcon from "../techno_icon";
 import "./index.scss";
 
 function ChartBar(props) {
 
-    const fill = props.value * 5;
+    const fill = props.value * props.multiplier;
 
     return (
         <div className="chart_bar">
             <TechnoIcon name={props.name} />
             <svg id="chart_bar_svg" xmlns="http://www.w3.org/2000/svg">
-                <rect className="chart_empty" width="500" height="19" />
+                <rect className="chart_empty" width={props.width} height="19" />
                 <rect className="chart_filled" width={fill} height="15" y="2" />
             </svg>
             <p className="percentage">{`${props.value}%`}</p>
--- src/sections/skills/index.jsx
+++ src/sections/skills/index.jsx
@@ -2,8 +2,26 @@ import "./index.scss";
 import oc_logo from "../../images/oc_logo.jpg"
 import { Link } from "react-router-dom";
 import ChartBar from "../../components/chart_bar";
+import { useState } from "react";
 
 function Skills() {
+
+    const [getWidth, setWidth] = useState(500);
+
+    function updateSize() {
+        const newWidth = window.innerWidth;
+        if (newWidth > 700 && getWidth !== 500) {
+            setWidth(500);
+        } else if (newWidth < 700 && newWidth > 440 && getWidth !== 300) {
+            setWidth(300);
+        } else if (newWidth < 440 && getWidth !== 200) {
+            setWidth(200);
+        }
+    }
+
+    updateSize();
+    window.addEventListener("resize", updateSize);
+    const multiplier = getWidth / 100;
 
     return (
         <section id="skills">
@@ -20,16 +38,16 @@ function Skills() {
             <div className="skills_left">
                 <div className="chart_list_border" />
                 <div className="chart_list">
-                    <ChartBar key="HTML" name="HTML" value="77" />
-                    <ChartBar key="CSS" name="CSS" value="62" />
-                    <ChartBar key="Javascript" name="Javascript" value="44" />
-                    <ChartBar key="Sass" name="Sass" value="15" />
-                    <ChartBar key="React" name="React" value="38" />
-                    <ChartBar key="Redux" name="Redux" value="24" />
-                    <ChartBar key="SEO" name="SEO" value="55" />
-                    <ChartBar key="Jest" name="Jest" value="23" />
-                    <ChartBar key="Next" name="Next" value="0" />
-                    <ChartBar key="Typescript" name="Typescript" value="10" />
+                    <ChartBar key="HTML" name="HTML" value="77" width={getWidth} multiplier={multiplier} />
+                    <ChartBar key="CSS" name="CSS" value="62" width={getWidth} multiplier={multiplier} />
+                    <ChartBar key="Javascript" name="Javascript" value="44" width={getWidth} multiplier={multiplier} />
+                    <ChartBar key="Sass" name="Sass" value="15" width={getWidth} multiplier={multiplier} />
+                    <ChartBar key="React" name="React" value="38" width={getWidth} multiplier={multiplier} />
+                    <ChartBar key="Redux" name="Redux" value="24" width={getWidth} multiplier={multiplier} />
+                    <ChartBar key="SEO" name="SEO" value="55" width={getWidth} multiplier={multiplier} />
+                    <ChartBar key="Jest" name="Jest" value="23" width={getWidth} multiplier={multiplier} />
+                    <ChartBar key="Next" name="Next" value="0" width={getWidth} multiplier={multiplier} />
+                    <ChartBar key="Typescript" name="Typescript" value="10" width={getWidth} multiplier={multiplier} />
                 </div>
             </div>
         </section>
