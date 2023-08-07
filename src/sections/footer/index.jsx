8b51d88d5983fe8a79939f959d93d1083353f189
Maxence Lesage <maxencelesage4@gmail.com>
2023-08-07 16:01:06 +0200

    Adding social media links + fiverr card icon bug fix

--- src/sections/footer/index.jsx
+++ src/sections/footer/index.jsx
@@ -1,9 +1,23 @@
 import "./index.scss";
+import youtube from "../../images/youtube.png";
+import instagram from "../../images/instagram.png";
+import fiverr from "../../images/fiverr_logo.png";
 
 function Footer() {
     return (
         <footer className="footer">
-            © Site créé en React.js, repository <a href="https://github.com/Maxence-Lesage/Portfolio.git">à cette adresse</a>.
+            <p>© Site créé en React.js, repository <a href="https://github.com/Maxence-Lesage/Portfolio.git">à cette adresse</a>.</p>
+            <div className="social_media">
+                <a href="https://www.youtube.com/@SleekSite">
+                    <img src={youtube} alt="Logo de Youtube" />
+                </a>
+                <a href="https://www.instagram.com/sleeksiteen">
+                    <img src={instagram} alt="Logo d'Instagram" />
+                </a>
+                <a href="/">
+                    <img src={fiverr} alt="Logo de Fiverr" />
+                </a>
+            </div>
         </footer>
     )
 }
