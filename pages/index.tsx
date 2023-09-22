59aba1b20f6724a2c00487bbee4e63e6abc7876d
Maxence Lesage <maxencelesage4@gmail.com>
2023-09-22 19:53:15 +0200

    Adding Basic <Head> in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,17 @@ import Projects from '@/sections/projects/projects'
 import Skills from '@/sections/skills/skills'
 import Footer from '@/sections/footer/footer'
 import Modal from '@/components/contact/modal'
+import Head from 'next/head'
 
 export default function Home() {
     return (
         <>
+            <Head>
+                <title>Sleek Site</title>
+                <meta name="description" content="Portfolio de Maxence Lesage développeur frontend" />
+                <meta name="author" content="Maxence Lesage" />
+            </Head>
+
             <Navbar />
             <Hero />
             <Projects />
